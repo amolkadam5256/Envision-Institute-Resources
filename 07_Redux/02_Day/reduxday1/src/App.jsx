e4ee63c563a68b6163cea24db4506260d5557d31
amolkadam5256 <amolkadam1274@gmail.com>
2025-02-27 16:56:30 +0530

    Added counter app using redux to use state managment

--- 07_Redux/02_Day/reduxday1/src/App.jsx
+++ 07_Redux/02_Day/reduxday1/src/App.jsx
@@ -4,10 +4,10 @@ import Counter from './Components/counter'
 const App = () => {
   return (
     <>
-    <h1>Redux in React Counter Ex </h1>
-    <Counter/>
+      <h1>Redux in React Counter Ex </h1>
+      <Counter />
     </>
-  )
+  );
 }
 
 export default App
