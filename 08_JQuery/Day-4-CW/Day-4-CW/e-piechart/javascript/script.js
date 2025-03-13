c3dcc5cd01ee91e09ef679405190d270d85ebe77
amolkadam5256 <amolkadam1274@gmail.com>
2025-03-13 12:53:01 +0530

    Added day 3 to Day 5

---
+++ 08_JQuery/Day-4-CW/Day-4-CW/e-piechart/javascript/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.chart').easyPieChart({
+      barColor : "#F05A7E",
+      trackColor : "#3652AD50",
+      lineWidth : 20,
+      lineCap : 'butt',
+      scaleLength : 0,
+      size : 300,
+      rotate : 270
+    });
+  });
